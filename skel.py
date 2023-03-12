adf76c38448e83fc7ccec653f644d724082df229
David Parro <david.parro.sp@gmail.com>
2023-03-12 13:26:23 +0100

    funcional, basico

--- skel.py
+++ skel.py
@@ -9,9 +9,10 @@ from multiprocessing import Value, Array
 SOUTH = 1
 NORTH = 0
 
-NCARS = 100
-NPED = 10
-TIME_CARS = 0.5  # a new car enters each 0.5s
+NCARS = 10
+NPED = 2
+TIME_CARS_NORTH = 0.5  # a new car enters each 0.5s
+TIME_CARS_SOUTH = 0.5  # a new car enters each 0.5s
 TIME_PED = 5 # a new pedestrian enters each 5s
 TIME_IN_BRIDGE_CARS = (1, 0.5) # normal 1s, 0.5s
 TIME_IN_BRIDGE_PEDESTRGIAN = (30, 10) # normal 1s, 0.5s
@@ -39,61 +40,74 @@ extension_4:
 class Monitor():
     def __init__(self):
         self.mutex = Lock()
-        self.personas = Value('i', 0)
-        self.l_coches = Array('i',2)
-        # self.lock_dentro = Lock()
+        self.patata = Value('i', 0)
+        self.w_carriles_c = Array('i', 2)
+        self.coches = Value('i',0)
+        self.n_peatones_dentro    = Value('i',0)
+        self.n_peatones_waiting   = Value('i', 0)
+        self.no_coches = Condition(self.mutex)
+        self.vacio     = Condition(self.mutex)
+
+
+    def esta_vacio(self):
+        return self.coches.value == 0 and \
+        self.n_peatones_waiting.value == 0
 
     def wants_enter_car(self, direction: int) -> None:
         self.mutex.acquire()
-        self.l_coches[direction] += 1
+        self.patata.value += 1
         #### código
+        self.w_carriles_c[direction] += 1
+        self.vacio.wait_for(self.esta_vacio)
+        self.w_carriles_c[direction] -= 1
+        self.coches.value += 1
         self.mutex.release()
 
     def leaves_car(self, direction: int) -> None:
         self.mutex.acquire() 
-        self.l_coches[direction] -= 1
+        self.patata.value += 1
+        self.coches.value -=1
+        self.vacio.notify()
+        self.no_coches.notify_all()
         #### código
-        # self.lock_dentro.release()
         self.mutex.release()
 
+    def are_no_cars(self):
+        return self.coches.value == 0
+    
     def wants_enter_pedestrian(self) -> None:
         self.mutex.acquire()
-        self.personas.value += 1
+        self.patata.value += 1
+        self.n_peatones_waiting.value += 1
         #### código
+        self.no_coches.wait_for(self.are_no_cars)
+        self.n_peatones_dentro.value  += 1
+        self.n_peatones_waiting.value -= 1
         self.mutex.release()
-    
-    # def is_inside(self) -> None:
-    #     self.mutex.acquire()
-    #     self.lock_dentro.acquire()
-    #     self.mutex.release()
 
     def leaves_pedestrian(self) -> None:
         self.mutex.acquire()
-        self.personas.value  -= 1
+        self.patata.value += 1
         #### código
-        # self.lock_dentro.release()
+        if self.n_peatones_waiting == 0:
+            self.vacio.notify()
         self.mutex.release()
 
     def __repr__(self) -> str:
-        return f'Monitor: {self.personas.value}'
+        return f'Monitor: {self.patata.value}'
 
 def delay_car_north() -> None:
-    time.sleep(random.random())
+    pass
 
 def delay_car_south() -> None:
-    time.sleep(random.random())
-
+    pass
 
 def delay_pedestrian() -> None:
-    time.sleep(random.random())
-
+    pass
 
 def car(cid: int, direction: int, monitor: Monitor)  -> None:
     print(f"car {cid} heading {direction} wants to enter. {monitor}")
     monitor.wants_enter_car(direction)
-    
-    # Garantizar exclusion mutua de esto
-    # monitor.is_inside()
     print(f"car {cid} heading {direction} enters the bridge. {monitor}")
     if direction==NORTH :
         delay_car_north()
@@ -101,14 +115,11 @@ def car(cid: int, direction: int, monitor: Monitor)  -> None:
         delay_car_south()
     print(f"car {cid} heading {direction} leaving the bridge. {monitor}")
     monitor.leaves_car(direction)
-
     print(f"car {cid} heading {direction} out of the bridge. {monitor}")
 
 def pedestrian(pid: int, monitor: Monitor) -> None:
     print(f"pedestrian {pid} wants to enter. {monitor}")
     monitor.wants_enter_pedestrian()
-    # Garantizar exclusion mutua de esto
-    # monitor.is_inside()
     print(f"pedestrian {pid} enters the bridge. {monitor}")
     delay_pedestrian()
     print(f"pedestrian {pid} leaving the bridge. {monitor}")
@@ -130,27 +141,29 @@ def gen_pedestrian(monitor: Monitor) -> None:
     for p in plst:
         p.join()
 
-def gen_cars(monitor: Monitor) -> None:
+def gen_cars(direction: int, time_cars, monitor: Monitor) -> None:
     cid = 0
     plst = []
     for _ in range(NCARS):
-        direction = NORTH if random.randint(0,1)==1  else SOUTH
         cid += 1
         p = Process(target=car, args=(cid, direction, monitor))
         p.start()
         plst.append(p)
-        time.sleep(random.expovariate(1/TIME_CARS))
+        time.sleep(random.expovariate(1/time_cars))
 
     for p in plst:
         p.join()
 
 def main():
     monitor = Monitor()
-    gcars = Process(target=gen_cars, args=(monitor,))
+    gcars_north = Process(target=gen_cars, args=(NORTH, TIME_CARS_NORTH, monitor))
+    gcars_south = Process(target=gen_cars, args=(SOUTH, TIME_CARS_SOUTH, monitor))
     gped = Process(target=gen_pedestrian, args=(monitor,))
-    gcars.start()
+    gcars_north.start()
+    gcars_south.start()
     gped.start()
-    gcars.join()
+    gcars_north.join()
+    gcars_south.join()
     gped.join()
 
 
